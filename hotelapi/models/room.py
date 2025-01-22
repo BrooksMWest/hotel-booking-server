cba2efb62de636505e578a333d069fdcffce8e1f
BrooksMWest <brooksmwest@hotmail.com>
2025-01-21 20:26:26 -0600

    work in progress room model

---
+++ hotelapi/models/room.py
@@ -0,0 +1,13 @@
+from django.db import models
+from hotelapi.models.booking import Booking
+
+class Room(models.Model):
+
+  room_number = models.CharField()
+  vacancy = models.BooleanField()
+  room_size = models.CharField(max_length=25)
+  star_rating = models.CharField(max_length=5)
+  price = models.CharField(max_length=10)
+  good_view = models.BooleanField()
+  smoking = models.BooleanField()
+  booking = models.ForeignKey(Booking, on_delete=models.CASCADE)
