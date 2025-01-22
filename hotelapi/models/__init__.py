5bb677ab48974633d18d4498107e3197dd320620
BrooksMWest <brooksmwest@hotmail.com>
2025-01-21 21:00:06 -0600

    fixed room model and event made singular

--- hotelapi/models/__init__.py
+++ hotelapi/models/__init__.py
@@ -1,4 +1,5 @@
 from .customer import Customer
 from .booking import Booking
 from .customer_booking import CustomerBooking
-from .events import Events
+from .event import Events
+from .room import Room
