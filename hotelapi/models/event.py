27e0702e477a7b286548d6208349a5d755adf3a6
Brooks West <brooksmwest@hotmail.com>
2025-01-27 19:28:24 -0600

    added fields to the event model

--- hotelapi/models/event.py
+++ hotelapi/models/event.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Event(models.Model):
     event_name = models.CharField(max_length=255)
+    description = models.CharField(max_length=500)
+    image_url = models.CharField(max_length=255)
     date = models.DateTimeField()
     time = models.TimeField()
 
